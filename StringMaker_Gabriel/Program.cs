26b545598c7c9f7b3c3dfca80257adf1dbf46232
Keri <79213053+KeriGabriel@users.noreply.github.com>
2021-11-30 14:59:14 -0800

    Add project files.

---
+++ StringMaker_Gabriel/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+// Keri Gabriel
+// IT113
+// NOTES: notes the instructor should read
+// BEHAVIORS NOT IMPLIMENTED AND WHY: none
+
+namespace StringMaker_Gabriel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringManager S = new StringManager();
+          
+            reverseAstring();
+            callToString();        
+            reverseAstring2();
+            equalityCheck();
+            symmetricCheck();         
+
+            void callToString()
+            {
+                Console.WriteLine("Call ToString to output the string equivalent of all ASCII summed values ");
+                Console.WriteLine("");
+                S.Reverse("Sunbeam Tiger");
+                Console.WriteLine(S);
+                Console.WriteLine("");
+                //string newString = "Dan";
+                //newString = S.getValue("Dan").ToString();
+            }
+            void reverseAstring()
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Reverse a string Sunbeam Tiger ");
+                Console.WriteLine("");                
+                Console.WriteLine(S.Reverse("Sunbeam Tiger"));
+                Console.WriteLine("");
+            }  
+            void reverseAstring2()
+            {
+                //Reverse a string "Sunbeam Tiger" preserving casing location in the string
+                Console.WriteLine("Reverse a string Sunbeam Tiger preserving casing location in the string");
+                Console.WriteLine("");
+                string ReverseAstring2 = S.Reverse("Sunbeam Tiger", true);
+                string ReverseAstring3 = S.Reverse("Sunbeam tiGer", true);
+                string ReverseAstring4 = S.Reverse("SunBEAM tiGer", true);
+                Console.WriteLine("Original string Sunbeam Tiger: " + ReverseAstring2);
+                Console.WriteLine("Original string Sunbeam tiGer: " + ReverseAstring3);
+                Console.WriteLine("Original string  SunBEAM tiGer: " + ReverseAstring4);
+                Console.WriteLine("");
+            }
+            void equalityCheck() 
+            {
+                S.Reverse("Sunbeam Tiger");
+                //Equality Check call for "Sunbeam Tiger" ***needs work***
+                Console.WriteLine("Equality Check call for Sunbeam Tiger");
+                Console.WriteLine("");
+                Console.WriteLine(" Equals " + S.Equals("Sunbeam Tiger"));               
+                Console.WriteLine("");
+            }        
+            void symmetricCheck() 
+            {
+                Console.WriteLine("Symmetric check");
+                Console.WriteLine("");
+                //true
+                bool check1 = S.Symmetric("ABBA");
+                Console.WriteLine("Symmetric check for ABBA: " + check1);
+                //true
+                bool check2 = S.Symmetric("ABA");
+                Console.WriteLine("Symmetric check for ABA: " + check2);
+                //false
+                bool check3 = S.Symmetric("ABba");
+                Console.WriteLine("Symmetric check for ABba: " + check3);
+                //false
+                bool check4 = S.Symmetric("jhyu");
+                Console.WriteLine("Symmetric check for jhyu: " + check4);
+                Console.WriteLine("");
+            }
+  
+        }
+    }
+}
---
+++ StringMaker_Gabriel/StringManager.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+
+namespace StringMaker_Gabriel
+{
+    class StringManager
+    {
+        private string _value;
+        int sum = 0;
+        string temp;
+
+        public string Reverse(string s)
+        {
+            _value = s;
+            //var for final reversed string
+            string answer = string.Empty;
+
+            // loop through and switch 
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                //string IS a Char array
+                answer += s[i];
+            }
+            return answer;
+        }
+       
+        public string Reverse(string s, bool t) 
+        {
+            _value = s;
+            //split words put into array
+            string[] arr = s.Split(' ');
+            //var for answer
+            string answer = string.Empty;
+            //foreach 
+            foreach (var c in arr)
+            {
+                //for loop index c length -1 to start
+                for (int i = c.Length - 1; i >= 0; i--)
+                {
+                    // if char is upper i and length -1
+                    if (Char.IsUpper(c[c.Length - 1 - i]))
+                        // answer = answer+current index to upper
+                        answer += c[i].ToString().ToUpper();
+                    else
+                        //else answer = answer+current index to lower
+                        answer += c[i].ToString().ToLower();
+                }
+                //add spaces
+                answer += " ";
+            }
+            return answer;
+        }
+           
+        public bool Symmetric(string s)
+        {
+            //index
+            int i = 0;
+            //length
+            int j = s.Length - 1;
+            _value = s; 
+
+            //loop           
+            while (true)
+            {
+                //if index is less than length keep going
+                if (i > j)
+                {
+                    return true;
+                }
+                // assign var to each letter to compare
+                char a = s[i];
+                char b = s[j];
+                //if the do not match return false it's not Symmetric 
+                //**Note  if case insensitive (char.ToLower(a) != char.ToLower(b))**
+                if (a != b)
+                {
+                    return false;
+                }
+                //increase index decrease length
+                i++;
+                j--;
+            }
+        }
+        public override bool Equals(object obj)
+        {
+            //only return true if obj is string
+            if (obj is string)
+            {
+                //if obj does not = string value return false, otherwise return true
+                if (obj != _value)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }              
+            }
+            //everything else return false
+            else
+            {
+                return false;
+            }
+        }       
+    
+        public override string ToString()
+        {
+            //new dictionary
+            Dictionary<int, string> Key = new Dictionary<int, string>();
+            //temp var for sum 
+            string tempSum = string.Empty;
+            //var for length
+            int length = _value.Length;
+            //loop through and add 
+            for (int i = 0; i < length; i++)
+            {
+                sum += _value[i];
+            }
+            //make a var to convert sum toString
+            tempSum = sum.ToString();
+            //Key 
+            Key.Add(49, "One");
+            Key.Add(50, "Two");
+            Key.Add(51, "Three");
+            Key.Add(52, "Four");
+            Key.Add(53, "Five");
+            Key.Add(54, "Six");
+            Key.Add(55, "Seven");
+            Key.Add(56, "Eight");
+            Key.Add(57, "Nine");
+            Key.Add(48, "Zero");
+            Key.Add(45, "Negative One");
+
+            //loop through and switch and add spaces
+            for (int i = 0; i < tempSum.Length; i++)
+            {
+               temp += Key[tempSum[i]]+" ";
+            }
+           //return temp
+            return temp;
+        }   
+    }
+}
+
+
